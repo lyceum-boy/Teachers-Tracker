1e9aa51f915dd34b20b91016e14c194850ed84ee
Ilya Anosov <anosovilya465@yandex.ru>
2021-05-13 13:17:29 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Teachers-Tracker.
+
+A flask app with social network bots that informs about teachers
+absence and substitute teachers.
+"""
+
+from flask import Flask
+
+__author__ = ["Ilya B. Anosov", "Sofia P. Kalinina"]
+__credits__ = ["Georgiy A. Darovskih"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Ilya B. Anosov"
+__email__ = "anosovilya465@yandex.ru"
+__status__ = "Development"
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
