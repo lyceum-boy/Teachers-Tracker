34a38a4a41b0c248b33fd4c33e19b1eb9043c7e8
Ilya Anosov <anosovilya465@yandex.ru>
2021-05-14 14:28:50 +0900

    Add static/img/favicon.svg

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ absence and substitute teachers.
 import random
 
 from flask import Flask
+from flask import redirect
 from flask import render_template
 
 from flask_login import LoginManager
@@ -22,6 +23,9 @@ from data import db_session
 from data.groups import Group
 from data.students import Student
 from data.teachers import Teacher
+
+from forms.login import LoginForm
+from forms.user import RegisterForm
 
 __author__ = "Ilya B. Anosov"
 __credits__ = ["Maria A. Zamaryokhina", "Sofia P. Kalinina"]
@@ -59,6 +63,71 @@ def index():
              for teacher in teachers}
     return render_template('index.html', teachers=teachers, names=names,
                            title='Управление системой | Teachers-Tracker')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template(
+                'register.html',
+                title='Регистрация | Teachers-Tracker',
+                form=form,
+                message="Пароли не совпадают"
+            )
+        db_sess = db_session.create_session()
+        if db_sess.query(Teacher).filter(
+                Teacher.email == form.email.data).first():
+            return render_template(
+                'register.html',
+                title='Регистрация | Teachers-Tracker',
+                form=form,
+                message="Такой пользователь уже есть"
+            )
+        teacher = Teacher()
+        teacher.surname = form.surname.data
+        teacher.name = form.name.data
+        teacher.middle_name = form.middle_name.data
+        teacher.age = form.age.data
+        teacher.address = form.address.data
+        teacher.phone = form.phone.data
+        teacher.email = form.email.data
+        teacher.education = form.education.data
+        teacher.set_password(form.password.data)
+        db_sess.add(teacher)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html',
+                           title='Регистрация | Teachers-Tracker',
+                           form=form
+                           )
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(Teacher).filter(
+            Teacher.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html',
+                           title='Авторизация | Teachers-Tracker',
+                           form=form
+                           )
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 def auth_handler():
